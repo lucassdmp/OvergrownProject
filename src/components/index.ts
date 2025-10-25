a7fabe752ca79cf83e4f867ae2461cde30c1d3c8
Lucas <lucassdmp01@gmail.com>
2025-10-24 22:01:16 -0300

    refactor: remove unused components, hooks, and styles; add .env template

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-// Export all components from this file for easier imports
-export { default as Button } from './Button';
--- src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-// Export all custom hooks
-export { useLocalStorage } from './useLocalStorage';
